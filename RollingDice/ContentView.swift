0c5c316851bcdc722987b257aaa1bf626db21346
Henny2 <henrieke.baunack@web.de>
2024-06-01 16:58:47 -1000

    adding selection for type of dice

--- RollingDice/ContentView.swift
+++ RollingDice/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceRolls = []
+    let diceTypes = [6, 8, 12, 20, 100]
+    @State private var rolledNum = 0
+    // number of sides on the die
+    @State private var numSidesOfDice = 6 // default six sides
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("Select a dice type!")
+                Picker("Select a dice", selection: $numSidesOfDice){
+                    ForEach(diceTypes, id:\.self){
+                        Text("\($0)")
+                    }
+                }
+            }
+            Button("Roll dice"){
+                rolledNum = Int.random(in: 1..<numSidesOfDice)
+                diceRolls.append(rolledNum)
+            }
+            if(rolledNum > 0){
+                Text("Rolled Number: \(rolledNum)")
+            }
+            Text("Rolled Nums: \(diceRolls)")
         }
         .padding()
     }
