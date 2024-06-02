e6ac8f7f240c3eb62618214cea0206b5ff37283c
Henny2 <henrieke.baunack@web.de>
2024-06-01 16:35:54 -1000

    Initial Commit

---
+++ RollingDice/RollingDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingDiceApp.swift
+//  RollingDice
+//
+//  Created by Henrieke Baunack on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RollingDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
